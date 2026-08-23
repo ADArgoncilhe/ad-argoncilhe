c0907ef35eafe162797edc58bd6996a68ed9db92
ADArgoncilhe <gui_oliveira@hotmail.com>
2026-08-23 23:51:29 +0100

    Delete ad-argoncilhe-site directory

--- ad-argoncilhe-site/config.js
+++
@@ -1,4 +0,0 @@
-// Preencher com os dados do teu projeto Supabase.
-// Usa apenas a Publishable/Anon key. NUNCA coloques a service_role key aqui.
-window.SUPABASE_URL = 'https://TEU-PROJETO.supabase.co';
-window.SUPABASE_KEY = 'COLOCA_AQUI_A_TUA_PUBLISHABLE_KEY';
